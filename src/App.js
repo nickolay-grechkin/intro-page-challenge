dcd39684fb477ec305717edc200a7c76e56ce6dc
nickolay-grechkin <nickolaygrechkin@gmail.com>
2022-07-22 20:22:54 +0300

    Addapted design to mobile resolution.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import styles from './styles.module.scss';
 import heroImage from './images/image-hero-desktop.png'
+import heroImageMobile from './images/image-hero-mobile.png'
+import {ReactComponent as Audiophile} from "./images/client-audiophile.svg";
+import {ReactComponent as Databiz} from "./images/client-databiz.svg";
+import {ReactComponent as Maker} from "./images/client-maker.svg";
+import {ReactComponent as Meet} from "./images/client-meet.svg";
+import {useScreenWidth} from "./hooks/useScreenWidth";
 
 function App() {
+    const [isTabletResolution, isMobileResolution] = useScreenWidth();
   return (
     <div className={styles.appWrapper}>
       <div className={styles.appContainer}>
@@ -22,10 +29,16 @@ function App() {
                             Learn more
                           </div>
                       </div>
+                      <div className={styles.companiesIconsWrapper}>
+                            <Audiophile />
+                            <Databiz />
+                            <Meet />
+                            <Maker />
+                      </div>
                   </div>
                   <div className={styles.imageWrapper}>
                       <div className={styles.imageContainer}>
-                          <img src={heroImage} alt="" />
+                          <img src={isTabletResolution || isMobileResolution ? heroImageMobile : heroImage} alt="" />
                       </div>
                   </div>
               </div>
